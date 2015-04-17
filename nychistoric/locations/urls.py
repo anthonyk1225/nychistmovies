e01a66857a8637e92b9bc5cb6de9f8ec4fd59ba0
Connor Hammond <ConnorH2582@users.noreply.github.com>
2015-04-17 17:29:49 -0400

    Creates htmls, css, & js files, adds icon functionality (locations currently hardcoded)

--- nychistoric/locations/urls.py
+++ nychistoric/locations/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url,patterns
 from django.contrib import admin
+from locations.views import IndexView
+import locations.views as view
 
-urlpatterns = [
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'nychistoric.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^/', )),
-]
+    url(r'^',IndexView.as_view(),name='get'),
+)
--- nychistoric/locations/views.py
+++ nychistoric/locations/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+class IndexView(View):
+    template = 'locations/index.html'
+
+    def get(self, request):
+        return render(request,self.template)

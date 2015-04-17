40adcd1e07f2c76ef5a4ae3a7976c18a4bff4d42
Connor Hammond <ConnorH2582@users.noreply.github.com>
2015-04-17 18:24:25 -0400

    Fixes up js

--- nychistoric/locations/static/locations/base.js
+++ nychistoric/locations/static/locations/base.js
@@ -1,19 +1,20 @@
-$( document ).ready(function initialize() {
-        var mapOptions = {
-            zoom: 10,
-            center: new google.maps.LatLng(40.74727,-73.9800645)
-        }
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+function initialize() {
+    var mapOptions = {
+        zoom: 10,
+        center: new google.maps.LatLng(40.74727,-73.9800645)
+    }
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
 
-        var image = "static/locations/video.png"
-        var filmLatLng = new google.maps.LatLng(40.7604000000000,-73.9758000000000);
-        var beachMarker = new google.maps.Marker({
-            position: filmLatLng,
-            map: map,
-            icon: image
-        });
-    // };
+    var image = "static/locations/video.png"
+    var filmLatLng = new google.maps.LatLng(40.7604000000000,-73.9758000000000);
+    var beachMarker = new google.maps.Marker({
+        position: filmLatLng,
+        map: map,
+        icon: image
+    });
+}
+
+$( document ).ready(function() {
+    google.maps.event.addDomListener(window, 'load', initialize);
 });
-
-google.maps.event.addDomListener(window, 'load', initialize);

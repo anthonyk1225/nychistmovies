6e98890d39bb6cf6f4718ae52b22d9acc5cc5abd
Anthony Keating <anthonyk1225@gmail.com>
2015-04-20 01:51:02 -0400

    titles are now attached to the appropriate icon

--- nychistoric/locations/static/locations/base.js
+++ nychistoric/locations/static/locations/base.js
@@ -1,18 +1,21 @@
 $( document ).ready(function() {
         var mapOptions = {
-            zoom: 15,
-            center: new google.maps.LatLng(40.74727,-73.9800645)
+            zoom: 16,
+            center: new google.maps.LatLng(40.7517291,-73.9794923)
         }
         var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
         $.getJSON("/nycfilms/coords/", function(data){
+                            console.log(data['coords'])
+
             for (i=0;i<data['coords'].length; i++){
                 var image = "/static/locations/video.png"
                 var filmLatLng = new google.maps.LatLng(data['coords'][i][0],data['coords'][i][1]);
                 var beachMarker = new google.maps.Marker({
                     position: filmLatLng,
                     map: map,
-                    icon: image
+                    icon: image,
+                    title: data['coords'][i][2]
             });
         }
         })
